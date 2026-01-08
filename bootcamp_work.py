46260a8db8e41de04d3ee284f924761239403e4a
Oroz <orozmamat.arzymamatov@gmail.com>
2026-01-08 11:04:34 +0600

    машина боюнча Ðмаалымат коштум

--- bootcamp_work.py
+++ bootcamp_work.py
@@ -1,1 +1,11 @@
 print("Start Project")
+
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+mashina = Car("Tayota","Camry")
+print(mashina.brand)
+print(mashina.model)
