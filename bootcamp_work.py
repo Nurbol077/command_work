207d78d6677641576f09756a6a8179355103d46e
nurbek <nurbekmahmudov384@gmail.com>
2026-01-08 11:32:47 +0600

    student maalymat

--- bootcamp_work.py
+++ bootcamp_work.py
@@ -1,1 +1,14 @@
 print("Start Project")
+
+
+class Student:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+
+Student = Student('marat', 90)
+print(Student.name)
+print(Student.score)
+
+
