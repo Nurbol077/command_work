322f540b7532c762085ba70e653f6df08763e77b
Peri <moldalyeva.peku@gmail.com>
2026-01-08 10:55:59 +0600

    Жаныбарлар жонундо маалыматÑ коштумÐ

--- bootcamp_work.py
+++ bootcamp_work.py
@@ -1,1 +1,9 @@
 print("Start Project")
+class Animal :
+    def __init__(self,name,color):
+        self.name = name
+        self.color = color
+
+a = Animal("Dog" , "red")
+print(a.name)
+print(a.color)
