41094a231e30222ec8ceb6b70c348e420e07318e
nurbek <nurbekmahmudov384@gmail.com>
2026-01-08 11:33:28 +0600

    pust

--- bootcamp_work.py
+++ bootcamp_work.py
@@ -1,14 +1,1 @@
 print("Start Project")
-
-
-class Student:
-    def __init__(self, name, score):
-        self.name = name
-        self.score = score
-
-
-Student = Student('marat', 90)
-print(Student.name)
-print(Student.score)
-
-
